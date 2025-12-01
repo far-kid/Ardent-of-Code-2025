147c788a3bee825423f16b7268c84b9e20517ca1
far-kid <goyalkartikeya66@gmail.com>
2025-12-02 04:38:51 +0530

    day1_part1

---
+++ Day1/day1_dials_part1.py
@@ -0,0 +1,30 @@
+def calculate_vector(com :str)->int:
+    quantity = int(com[1:])
+    if com[0]=="R":
+        return quantity
+    else:
+        return -quantity
+    
+def vector_actual(vector_val)->int:
+    return (vector_val%100)
+
+def get_result(com :str)->int:
+    return vector_actual(calculate_vector(com))
+
+def main():
+    commands = []
+    with open("z:/input.txt") as f:
+        for line in f:
+            clean= line.rstrip('\n')
+            commands.append(clean)
+    value = 50
+    zero_count= 0
+    for com in commands:
+        value+=get_result(com=com)
+        value%=100
+        if value==0:
+            zero_count+=1
+    print(zero_count)
+    
+if __name__ == "__main__":
+    main()
